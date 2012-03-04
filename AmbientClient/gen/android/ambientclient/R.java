1c4310579e2eeaaad9d86d4324ce8edab261fcca
Swen Hutop <swenhutop@googlemail.com>
2012-03-04 23:44:44 +0100

    merge

--- AmbientClient/gen/android/ambientclient/R.java
+++ AmbientClient/gen/android/ambientclient/R.java
@@ -10,14 +10,34 @@ package android.ambientclient;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int colors=0x7f050000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int lightButton=0x7f060003;
+        public static final int listView1=0x7f060000;
+        public static final int saveConfigButton=0x7f060006;
+        public static final int temperatureButton=0x7f060005;
+        public static final int textView1=0x7f060002;
+        public static final int textView2=0x7f060004;
+        public static final int welcomeTextView=0x7f060001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int main_view=0x7f030000;
+        public static final int registration=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int button_cancel=0x7f040008;
+        public static final int button_save=0x7f040007;
+        public static final int button_save_config=0x7f040006;
         public static final int hello=0x7f040000;
+        public static final int title_main_activity=0x7f040003;
+        public static final int title_registration=0x7f040002;
+        public static final int txt_light=0x7f040004;
+        public static final int txt_temperature=0x7f040005;
     }
 }
