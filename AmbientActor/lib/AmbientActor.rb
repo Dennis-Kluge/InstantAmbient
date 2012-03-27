68ecb0ab408f945623891a8d7bac8f8bba1ad7b0
Horst Mumpitz <dennis.kluge@gmx.de>
2012-03-27 11:14:35 +0200

    first small version for actor, communication is working

--- AmbientActor/lib/AmbientActor.rb
+++ AmbientActor/lib/AmbientActor.rb
@@ -1,5 +1,24 @@
 require "AmbientActor/version"
+require "eventmachine"
 
-module AmbientActor
-  # Your code goes here...
+class ActorConnection < EventMachine::Connection
+   
+  def receive_data(data)
+    p data
+
+    #@broker.apply_data(data)
+
+    #send_data data
+    #close_connection_after_writing 
+  end
+
+  def unbind
+    p ' connection closed'
+  end
+
 end
+
+EventMachine.run {
+  EventMachine::start_server('127.0.0.1', 9123, ActorConnection)
+  puts "Starting actor..."
+}
